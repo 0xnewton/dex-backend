6374ef784a5650dd9dec50820fb34609c28bf985
0xnewton <0xnewton@protonmail.com>
2025-08-26 22:45:10 -0400

    export onUserCreated function

---
+++ functions/src/handlers/onUserCreated.ts
@@ -0,0 +1,17 @@
+import { user } from "firebase-functions/v1/auth";
+import { UserService } from "../services/user";
+import { logger } from "firebase-functions";
+
+export const onUserCreated = user().onCreate(async (user) => {
+  // Handle user creation
+  logger.info("User created", {
+    uid: user.uid,
+    user,
+  });
+  const userService: UserService = new UserService();
+  try {
+    await userService.onUserCreated(user);
+  } catch (error) {
+    logger.error("Error in onUserCreated", { error });
+  }
+});
