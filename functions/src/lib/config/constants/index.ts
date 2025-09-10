308449beb1fd3f67cccf08f80d5f7f3815193410
0xnewton <0xnewton@protonmail.com>
2025-09-10 00:38:55 -0400

    lint

--- functions/src/lib/config/constants/index.ts
+++ functions/src/lib/config/constants/index.ts
@@ -1,2 +1,2 @@
 export const PLATFORM_FEE_BPS = 100;
-export const MAX_BPS = 10000;  // 100%
+export const MAX_BPS = 10000; // 100%
---
+++ functions/src/services/swap/get-fee-amount.ts
@@ -0,0 +1,5 @@
+import { PLATFORM_FEE_BPS } from "../../lib/config/constants";
+
+export const getFeeAmount = async () => {
+  return PLATFORM_FEE_BPS;
+};
